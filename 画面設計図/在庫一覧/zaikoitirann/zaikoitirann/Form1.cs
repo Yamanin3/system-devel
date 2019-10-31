51f164a9072d03994e954bc136a4dc3ebe6bdb19
Yamanin3 <39796520+Yamanin3@users.noreply.github.com>
2019-10-31 14:35:28 +0900

    画面設計フォルダを追加

---
+++ 画面設計図/在庫一覧/zaikoitirann/zaikoitirann/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zaikoitirann
+{
+    public partial class gridview_s : Form
+    {
+        public gridview_s()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
