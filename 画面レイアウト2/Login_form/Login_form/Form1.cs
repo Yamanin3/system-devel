531a3f5435c1376bed86bc87083f42a3f18de342
Yamanin3 <39796520+Yamanin3@users.noreply.github.com>
2019-11-06 13:34:11 +0900

    画面レイアウト2にログインフォームを追加

---
+++ 画面レイアウト2/Login_form/Login_form/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Login_form.Static_Classes;
+
+namespace Login_form
+{
+    public partial class Login_Form : Form
+    {
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private Keys my_keycode;
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+            
+            // 接続をオープンにする
+             Login_form.Static_Classes.AC.openConnection();
+
+            // 接続をクローズする
+            // Login_form.Static_Classes.AC.closeConnection();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Focus();
+
+            Login_form.Static_Classes.AC.openConnection();
+
+            if ((string.IsNullOrEmpty(this.textBox1.Text.Trim())) || (string.IsNullOrEmpty(this.textBox2.Text.Trim())))
+            {
+                
+
+                MessageBox.Show("ユーザー名とパスワードを入力してください", "ログイン : データ入力エラー", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                if (this.textBox1.CanSelect)
+                {
+                    this.textBox1.Select();
+                    this.textBox1.SelectAll();
+                    this.textBox2.Text = string.Empty;
+
+                }
+                return;
+
+                this.textBox1.SelectAll();
+                this.textBox2.Text = string.Empty;
+
+            }
+
+            AC.sql = "select ユーザー名, パスワード from table1 where ユーザー名 = @us and パスワード = @pa;";
+            AC.cmd.Parameters.Clear();
+            AC.cmd.CommandType = CommandType.Text;
+            AC.cmd.CommandText = AC.sql;
+
+            AC.cmd.Parameters.AddWithValue("@us", this.textBox1.Text.Trim().ToString());
+            AC.cmd.Parameters.AddWithValue("@pa", this.textBox2.Text.Trim().ToString());
+
+            AC.rd = AC.cmd.ExecuteReader();
+
+            if (AC.rd.HasRows)
+            {
+                while (AC.rd.Read())
+                {
+                    AC.currentFullName = AC.rd[0].ToString();
+                    MessageBox.Show("ようこそ " + AC.currentFullName, "ログイン : ログイン成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                this.textBox1.Text = string.Empty;
+                this.textBox2.Text = string.Empty;
+
+                this.Hide();
+
+
+
+             
+
+            }
+            else
+            {
+                MessageBox.Show("ユーザー名かパスワードが違います。もう一度やり直してください。", "ログイン : ログイン失敗", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                if (this.textBox1.CanSelect)
+                {
+                    this.textBox1.Select();
+                }
+            }
+            this.textBox1.SelectAll();
+            this.textBox2.Text = string.Empty;
+
+            AC.rd.Close();
+            AC.closeConnection();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void Login_Form_Activated(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+    }
+}
---
+++ 画面レイアウト2/Login_form/Login_form/Static_Classes/AccessDb_Connection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Login_form.Static_Classes
+{
+    class AC
+    {
+
+        public static OleDbConnection con = new OleDbConnection();
+        public static OleDbCommand cmd = new OleDbCommand("", con);
+        public static OleDbDataReader rd;
+
+        public static string currentFullName;
+        public static string sql;
+
+        public static string getConnectionString()
+        {
+            // Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Yamanin3\Desktop\学習\DB\Login_DB.mdb
+
+            string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + Application.StartupPath + "\\Login_DB.mdb;";
+
+            return connectionString;
+
+        }
+
+        public static void openConnection()
+        {
+
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.ConnectionString = getConnectionString();
+                    con.Open();
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show("システムエラー" + Environment.NewLine + "説明: " + ex.Message.ToString(), "ログイン : データベースへの接続", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public static void closeConnection()
+        {
+
+            try
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("システムエラー" + Environment.NewLine + "説明: " + ex.Message.ToString(), "ログイン : データベースへの接続", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+    }
+}
